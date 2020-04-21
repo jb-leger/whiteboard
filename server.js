a151d80bfed9c13aa5028c876a500a5045ff30c5
Florent Chehab <perso@floflo.ch>
2020-04-21 21:28:20 +0200

    feat(server): volatile uncompressed broadcast for better performance

--- server.js
+++ server.js
@@ -199,14 +199,14 @@ io.on('connection', function (socket) {
         if (smallestScreenResolutions && smallestScreenResolutions[whiteboardId] && socket && socket.id) {
             delete smallestScreenResolutions[whiteboardId][socket.id];
         }
-        socket.broadcast.emit('refreshUserBadges', null); //Removes old user Badges
+        socket.compress(false).volatile.broadcast.emit('refreshUserBadges', null); //Removes old user Badges
         sendSmallestScreenResolution();
     });
 
     socket.on('drawToWhiteboard', function (content) {
         content = escapeAllContentStrings(content);
         if (accessToken === "" || accessToken == content["at"]) {
-            socket.broadcast.to(whiteboardId).emit('drawToWhiteboard', content); //Send to all users in the room (not own socket)
+            socket.compress(false).volatile.broadcast.to(whiteboardId).emit('drawToWhiteboard', content); //Send to all users in the room (not own socket)
             s_whiteboard.handleEventsAndData(content); //save whiteboardchanges on the server
         } else {
             socket.emit('wrongAccessToken', true);
